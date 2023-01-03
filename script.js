a04cefea54e6004ba58bbf4c10c2b6909fc482bd
Rajdeep Das <dasrajdeep0306@gmail.com>
2023-01-04 01:20:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,23 +24,21 @@
 // Place any jQuery/helper plugins in here.
 
 $(document).ready(function(){
-	$(".contentsPanel").each(function() {                
-		$(this).prepend('<div class="hidePanel">[hide]</div><div class="showPanel">[show]</div>');
-	});
-	
-	
+    $(".contentsPanel").each(function() {                
+        $(this).prepend('<div class="hidePanel">[hide]</div><div class="showPanel">[show]</div>');
+    });
+    
+    
     $(".hidePanel").click(function(){
-		$( this ).siblings('ul').hide( 150, function() { 
-			$(this).parent().addClass('minimizedPanel');
-		});
+        $( this ).siblings('ul').hide( 150, function() { 
+            $(this).parent().addClass('minimizedPanel');
+        });
     });
     $(".showPanel").click(function(){
-		$( this ).siblings('ul').show( 150, function() { 
-			$(this).parent().removeClass('minimizedPanel');
-		});
+        $( this ).siblings('ul').show( 150, function() { 
+            $(this).parent().removeClass('minimizedPanel');
+        });
     });
-	
-	
+    
+    
 });
-
-
